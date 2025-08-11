7374c57840ea2fac66f7f144f8e0d2238387b05d
JOOYEONGJUN <galn7071@naver.com>
2025-08-11 11:49:27 +0900

    Crawling_Scraping

---
+++ Crawling_Scraping/index.js
@@ -0,0 +1,45 @@
+/*
+특정 패키지 (라이브러리) 를 설치
+nodeJS.com에 업로드 되어 있는 패키지 중에서
+현재 회사에서 필요한 패키지를 설치
+
+npm(기본모델) -> yarn(페이스북에서 개발한 npm 에 빠른 버전)
+
+패키지를 설치하기 위해서 초기 세팅 필요
+1.초기 세팅하는 방법
+npm init = 설치를 진행 하며 패키지 이름, 버전, 설명을 모두 개발자가 하나하나 설정
+
+npm ini -y = npm init 으로 설치할때 나오는 모든 질문에 미리 "네" 라고 자동응답하는 옵션
+
+만약에 npm  init 에서 -y를 붙이지 않았을 경우 
+
+*/
+/*
+npm init -y // 을 이용하여 기본 package 설치한다.
+npm install puppeteer // 크롤링 패키지 설치
+node index.js // 실행하기
+
+/*
+패키지 이름은 무엇인가요? (package name:)
+
+버전은 무엇인가요? (version:)
+
+설명은 어떻게 되나요? (description:)
+
+진입점(메인 파일)은 무엇인가요? (entry point:)
+
+테스트 명령어는요? (test command:)
+
+Git 저장소는요? (git repository:)
+
+키워드는요? (keywords:)
+*/
+
+/*
+txt 파일아냐 한글 파일이냐 와 같은 파이
+command prompt : 텍스트 기반 간단한 파일관리 , 기본적인 테트워크 명령어 수행
+(cmd)
+power shell : 편집이 가능한 디지털 파일을 다르는 최신 오피스
+*/
+// node index.js
+console.log("==== index.js 파일 실행하기====");
